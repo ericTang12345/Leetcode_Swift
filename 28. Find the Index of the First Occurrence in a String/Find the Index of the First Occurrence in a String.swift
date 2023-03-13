5f099e230ddddefd61fd1926229a3689aba8b2c0
EricTang <erictangappdev@gmail.com>
2023-03-14 04:14:26 +0800

    update at 2022.03.14

--- 28. Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String.swift
+++ 28. Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String.swift
@@ -14,37 +14,41 @@ class Solution {
             return -1
         }
         
-        var hIndex = 0
-        var nIndex = 0
+        var hPtr = 0
+        var nPtr = 0
         let hArray = Array(haystack)
         let nArray = Array(needle)
-        var isOccur = false
-        var occurIndex = -1
+        var isOccur = false // 搜尋到第一個字時為 true
+        var occurIndex = -1 // 搜尋到第一個字時的 index
         
-        while nIndex != nArray.count, hIndex != hArray.count {
-            if hArray.count - hIndex < nArray.count - nIndex {
+        while nPtr != nArray.count, hPtr != hArray.count {
+            // haystack 中如果已經檢查過的字段已經小於 needle 的字段量，即可判斷失敗
+            if hArray.count - hPtr < nArray.count - nPtr {
                 occurIndex = -1
                 break
             }
             
-            if hArray[hIndex] == nArray[nIndex] {
+            if hArray[hPtr] == nArray[nPtr] {
                 if isOccur == false {
+                    // 修改為已搜尋到字串並記錄位置
                     isOccur = true
-                    occurIndex = hIndex
+                    occurIndex = hPtr
                 }
-                hIndex += 1
-                nIndex += 1
+                hPtr += 1
+                nPtr += 1
             } else {
                 if isOccur {
-                    nIndex = 0
+                    // 將 haystack 的指標回到前一個紀錄搜尋到字串的位置
+                    hPtr = occurIndex
+                    // 字串檢查到某一個字時不相同，將參數重置
+                    nPtr = 0
                     isOccur = false
-                    hIndex = occurIndex
                     occurIndex = -1
                 }
-                hIndex += 1
+                hPtr += 1
             }
+            
         }
-        
         return occurIndex
     }
 }
